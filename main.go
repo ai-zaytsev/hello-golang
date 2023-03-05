9aa3d34f2531b0cd254c60b722b2f617e9968898
ai-zaytsev <83661744+ai-zaytsev@users.noreply.github.com>
2023-03-05 08:31:20 -0500

    Update main.go
    deleted /greet/

--- main.go
+++ main.go
@@ -10,10 +10,5 @@ func main() {
 		fmt.Fprintf(w, "Hello World")
 	})
 
-	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Path[len("/greet/"):]
-		fmt.Fprintf(w, "Hello %s\n", name)
-	})
-
 	http.ListenAndServe(":8080", nil)
 }
