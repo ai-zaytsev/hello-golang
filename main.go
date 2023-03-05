737a1432a057b7fd1990bb478fb92c0fc0a88d29
ai-zaytsev <83661744+ai-zaytsev@users.noreply.github.com>
2023-03-05 08:30:11 -0500

    Update main.go
    code refactoring

--- main.go
+++ main.go
@@ -1,22 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"os"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello World!</h1>"))
-}
-
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
 
-	mux := http.NewServeMux()
+	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Path[len("/greet/"):]
+		fmt.Fprintf(w, "Hello %s\n", name)
+	})
 
-	mux.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":"+port, mux)
+	http.ListenAndServe(":8080", nil)
 }
